0f15fd0f209b9f7d2502f9d6edbfe962964530f2
Elsad <6900@winchmore.enfield.sch.uk>
2026-04-05 20:02:07 +0100

    Update COURSE WORK.py

--- COURSE WORK.py
+++ COURSE WORK.py
@@ -6,57 +6,69 @@
 # Onions (Kg) (Done)
 # Ketchup (Litres) (Done)
 
+# START TIMER 
+import time 
+start = time.time() 
+
 # validation checking - so the code doesnt crash if file name is changed 
 # find the file (Hotdogs.txt) and make a chest to put our end results (final_data).
-def load_logs(file_needed = "hotdogs.txt"):
-  final_data = []
-  try:
-    hotdog_file = open(file_needed, "r")  
-# temporary give hotdogs.txt the nickname hotdogs_file
-# because we are trying this line of code python wont just crash.
-    for line in hotdog_file:
-      columns = line.strip().split(",")
-# this splits up the data everytime there is a comma becoming seperate lines.
+final_data = []
+with open('Hotdogs.txt', 'r') as file:
+  for line in file:
+# this splits up the data everytime there is a comma becoming seperate items.
+    items = line.strip().split(',')
 
-      if len(columns) == 7:
-# Validation 1: Vendor ID 
+    final_data.append(items)
+# Validation starts here.
+for i in (final_data):
+  
+# Validation 1: 
       /too do next:
-# validation 2: Name length, This checks if the Vendors name is actually valid.      
-        if len(colums[1]) < 2 or len(colums[1]) > 25:
-          print(f"Invalid! Your name is too short or long."!)
+  
+# Validation 2: DONE!
+  if len(i[1]) < 2 or len(i[1]) > 25:
+    print(f"Invalid! Your name is too short.")
+
 # Validation 3: Year and week 
       /too do next:
+
 # validation 4/5: Hotdog sales, This checks if both veg and meat hotdogs sold are divisable by 10.
-        if int(columns[3]) % 10 != 0:
+        if int(i[3]) % 10 != 0:
           print(f"Invalid! Hotdog sales have to end in a 0.")
-          continue 
-        if int(columns[4]) % 10 != 0:
+          
+        if int(i[4]) % 10 != 0:
           print(f"Invalid! Hotdog sales have to end in a 0.")
-          continue
+         
+          
 # Validation 6: Onion 
-        if float(columns[5]) <= 0
+        if float(i[5]) <= 0
+
 # validation 7: Ketchup, This checks if Ketchup must be between 1 and 4 Litres.
-        if int(columns[6]) <1 or int(columns[6]) > 4:
+        if int(i[6]) <1 or int(i[6]) > 4:
           print(f"Invalid! Ketchup must be between 1 and 4 litres.")
-          continue 
+         
         
         record = {
-          "V_ID": columns[0],
-          "V_Name": columns[1].lower()
-          "Year_and_week": columns[2],
-          "Veg_dogs_sold": int(columns[3]),
-          "Meat_dogs_sold": int(columns[4]),
-          "Onions": float(columns[5]),
-          "Ketchup_L": int(columns[6]),
+          "V_ID":(i[0]),
+          "V_Name":(i[1]) ,
+          "Year_and_week":(i[2]) ,
+          "Veg_dogs_sold":(i[3]),
+          "Meat_dogs_sold":(i[4]),
+          "Onions":(i[5]),
+          "Ketchup_L":(i[6]) ,
         }
         final_data.append(record)
 # (record) is basically the workers profiles. 
 # which is stored in the (final_data) a storage container.
-  hotdog_file.close()
+        final_data.append(record)
+                         
   print(f"Successfully loaded {len(final_data)} records.")
   return final_data
-# all this does is close the the actual hotdog.txt file and prints a message with organised profiles.
-
+# This closes the the actual hotdog.txt file and prints a message with organised profiles.
+# TIMER 
+end = time.time()
+length = end - start
+print("it took ", length, " seconds") 
 
 # linear search - this checks the items one by one in order until the item is matched.
 def linear_check (data, target_name):
