4b603b2dff50da81332da1f517480df1014acf72
Elsad <6900@winchmore.enfield.sch.uk>
2026-04-21 19:13:25 +0100

    Update COURSE WORK.py
    validation checks from 1-7 this is so that any data records that have errors which dont abide by the validation checks dont get saved to my valid_record storage. 
    this helps me because it allows me to disregard and "throw away" the unwanted data that has errors in them which are no use for me.
    Furether more i made my code print out which of the following records (data lines) passed the validation checks my code is also very robust because if it cannot load the data it doesnt crash rather it prints the message that the file that needs too be opened is not found.

--- COURSE WORK.py
+++ COURSE WORK.py
@@ -18,3 +18,44 @@ def load_data(filename):
         print("Could not find or read the file!")
 
     return final_data
+#~~~~~
+# VALIDATE DATA
+#~~~~~
+
+def validate_data(records):
+    valid_records = []
+    
+    for record in records:
+        # these lines of code check Vendor ID is uppercase, two letters, underscore, three digits e.g. AB_123
+        if len(record[0]) != 6 or not record[0][:2].isupper() or not record[0][:2].isalpha() or record[0][2] != '_' or not record[0][3:].isdigit():
+            continue
+        
+        # these lines of code check Vendor name is between 2 and 25 characters
+        if len(record[1]) < 2 or len(record[1]) >25:
+            continue
+
+        # these lines of code check if the Year and Week is in format YYYYWW and week is between 1 and 52
+        if len(record[2]) != 6 or not record[2].isdigit() or (int(record[2][3:]) >= 1 and int(record[2][3:]) <= 51): 
+            continue
+        
+        # these lines of code check if the veg dogs sold are divisible by 10
+        if int(record[3]) % 10 != 0:
+            continue
+
+        # these lines of code check if the meat dogs sold are divisible by 10
+        if int(record[4]) % 10 != 0:
+            continue
+        # these lines of code check if the onions are in Increments of for e.g. 0.5, 1.0, 1.5
+        if (float(record[5]) * 2).is_integer() == False:
+            continue
+        
+        # these lines of code check if the ketchup is an integer between 1 and 4
+        if int(record[6]) >= 1 and int(record[6]) <= 4:
+            # Because everything else has passed the checks this record is ready to be added to valid_records list
+            valid_records.append(record)
+
+
+    print("~ After the validation check only", len(valid_records), "out of", len(records), "records have passed sucessfully ~\n")
+    return valid_records
+
+
