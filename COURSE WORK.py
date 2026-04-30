fddbe3ebe6c1f8934426cee0c36189c622679783
Elsad <6900@winchmore.enfield.sch.uk>
2026-04-30 23:57:16 +0100

    COURSE WORK.py

--- COURSE WORK.py
+++ COURSE WORK.py
@@ -1,9 +1,9 @@
 import time
 
 #------------------
-# LOAD MY DATA
+# LOAD MY DATA 
 #------------------
-
+# this is where loading the file takes place. 
 def load_data(filename):
     final_data = []
     
@@ -21,34 +21,34 @@ def load_data(filename):
 records = load_data('Hotdogs.txt')
     
 #~~~~~
-# VALIDATE DATA
+# VALIDATE DATA this is where the validation checks are held.
 #~~~~~
 
 def validate_data(records):
     valid_records = []
-    
+    # these lines of code check Vendor ID is uppercase, two letters, underscore, three digits e.g. AB_123
     for record in records:
         if len(record[0]) != 6 or not record[0][:2].isupper() or not record[0][:2].isalpha() or record[0][2] != '_' or not record[0][3:].isdigit():
             continue
-        
+        # these lines of code check Vendor name is between 2 and 25 characters
         if len(record[1]) < 2 or len(record[1]) > 25:
             continue
-
+        # these lines of code check if the Year and Week is in format YYYYWW and week is between 1 and 52
         if len(record[2]) != 6 or not record[2].isdigit() or (int(record[2][3:]) >= 1 and int(record[2][3:]) <= 52): 
             continue
-        
+        # these lines of code check if the veg dogs sold are divisible by 10
         if int(record[3]) % 10 != 0:
             continue
-
+        # these lines of code check if the meat dogs sold are divisible by 10
         if int(record[4]) % 10 != 0:
             continue
-
+        # these lines of code check if the onions are in Increments of for e.g. 0.5, 1.0, 1.5
         if (float(record[5]) * 2).is_integer() == False:
             continue
-        
+        # these lines of code check if the ketchup is an integer between 1 and 4
         if int(record[6]) >= 1 and int(record[6]) <= 4:
             valid_records.append(record)
-
+     # Because everything else has passed the checks this record is ready to be added to valid_records list
     print("~ After the validation check only", len(valid_records), "out of", len(records), "records have passed sucessfully ~\n")
     return valid_records
 
@@ -56,7 +56,7 @@ valid_records = validate_data(records)
 
 
 #~~~~~
-# SEARCHING
+# SEARCHING this is where the searching functions are held
 #~~~~~
 
 def linear_search(records, options, higher_lower):
@@ -113,7 +113,7 @@ def binary_search_all(records, target, column):
     return []
 
 #~~~~~
-# Get_search_value
+# Get_search_value this is where the user gets to either enter a value they are looking for in the dadt storage or use the following value given in the list below to search for.
 #~~~~~
 
 def get_search_value(records, column):
@@ -143,7 +143,7 @@ def get_search_value(records, column):
     return input("Enter value: ")
 
 #~~~~~
-# SORTING
+# SORTING this is where the sorting functions are held.
 #~~~~~
 
 def bubble_sort(records, key):
@@ -176,7 +176,7 @@ def quick_sort(records, column):
 
 
 #~~~~~
-# PERFORMANCE TEST 
+# PERFORMANCE TEST this allows the user to choose when they want to see the comparison of the bubble sort/quick sort or the linear searches and binary search. 
 #~~~~~
 
 def performance_test(valid_records):
@@ -227,7 +227,7 @@ def performance_test(valid_records):
     print("===============\n")
 
 #~~~~~
-# HIGH LOW
+# HIGH LOW this is where the user can choose if they want the highest/lowest value or both to be outputted when picking their options. 
 #~~~~~
 
 def high_low(records, column, choice):
@@ -273,7 +273,7 @@ def high_low(records, column, choice):
 
 
 #~~~~~
-# SAVE RESULTS
+# SAVE RESULTS this is where the output of the options they pick will be saved onto a seperate .txt file. 
 #~~~~~
 
 def save_results_to_file(results, filename="results.txt"):
@@ -296,7 +296,7 @@ def save_results_to_file(results, filename="results.txt"):
 
 
 #~~~~~
-# MENU
+# MENU # this is where the user can chose what options they want to do and how the code works (they can control it) 
 #~~~~~
 def print_record(record):
     print(
